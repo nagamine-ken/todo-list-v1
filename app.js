b713db0343877e13caf20304b01ac0dc852cd602
Ken Nagamine <nagamine.digitalholding@gmail.com>
2023-03-19 19:31:42 -0700

    ToDo list v1, first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const date = require(__dirname + "/date.js")
+
+
+const app = express()
+app.set("view engine", "ejs")
+
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(express.static("public"))
+
+const items = ["Buy food", "Cook food"]
+const workItems = []
+
+app.get("/", function(req, res){
+    let day = date.getDate()
+    res.render('list', {listTitle: day, newListItems: items});
+})
+
+
+app.get("/work", function(req, res){
+    res.render('list', {listTitle: "Work List", newListItems: workItems});
+})
+
+
+app.post("/", function(req, res){
+    if (req.body.list === "Work"){
+        item = req.body.newItem
+        console.log(req.body)
+        workItems.push(item)
+        res.redirect("/work")
+    } else {
+        item = req.body.newItem
+        console.log(req.body)
+        items.push(item)
+        res.redirect("/")
+    }
+    
+})
+
+
+app.get("/about", function(req, res){
+    res.render("about");
+})
+
+// app.post("/work", function(req, res){
+       
+// })
+
+
+app.listen(3000, function(){
+    console.log("Listening to Port 3000!")
+})
